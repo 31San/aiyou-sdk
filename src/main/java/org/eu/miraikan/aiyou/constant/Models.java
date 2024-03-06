b57beb635b85c3f85e72b547ab02c5b91d5641d5
31San <326266235@qq.com>
2024-03-06 15:03:00 +0800

    update config info

--- src/main/java/org/eu/miraikan/aiyou/constant/Models.java
+++ src/main/java/org/eu/miraikan/aiyou/constant/Models.java
@@ -22,15 +22,20 @@ public class Models {
     public static final String GEMINI_PRO = "gemini-pro";
 
 
-    //Latest stable version. 2021?
+    //Latest stable version.
     public static final String GEMINI_1_0_PRO = "gemini-1.0-pro";
 
 
-    //Latest  version. 2023/02?
+    //Latest  version.
     public static final String GEMINI_1_0_PRO_LATEST = "gemini-1.0-pro-latest";
 
-    public static final String GEMINI_PRO_1_5 = "gemini-1.5-pro";
+    public static final String GEMINI_1_5_PRO = "gemini-1.5-pro";
 
+    public static final String GEMINI_1_5_PRO_LATEST = "gemini-1.5-pro-latest";
+
+    public static final String GEMINI_PRO_VISION  = "gemini-pro-vision";
+
+    public static final String GEMINI_1_0_PRO_VISION_LATEST  = "gemini-1.0-pro-vision-latest";
     //embedding
     public static final String EMBEDDING_001 = "models/embedding-001";
 
